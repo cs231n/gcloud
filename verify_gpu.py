8717b816103996705458c610a7b4ff3abba966e7
LinxiFan <jimfan@cs.stanford.edu>
2019-03-30 17:37:57 -0700

    update verify_gpu

--- verify_gpu.py
+++ verify_gpu.py
@@ -5,9 +5,8 @@ assert torch.cuda.is_available(), 'CUDA is not correctly installed!!'
 print(torch.tensor([2.0, 3.0, 1.0]).cuda())
 property = torch.cuda.get_device_properties(0)
 
-print('============= Your GPU info =============')
-print('name:\t', property.name)
-print('capability:\t', '{}.{}'.format(property.major, property.minor))
+print('====== Your GPU info ======')
+print('name:\t\t', property.name)
 print('capability:\t', 'v{}.{}'.format(property.major, property.minor))
-print('memory:\t', round(property.total_memory / 1e9), 'Gb')
+print('memory:\t\t', round(property.total_memory / 1e9), 'Gb')
 print('processors:\t', property.multi_processor_count)
